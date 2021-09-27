9394025b8a7f1e8c21ec68e3dada1e3eb67a3701
prsion <prsion@mail.ru>
2021-09-27 20:28:21 +0300

    lifecycle

--- src/mycomponent/Users/Users.jsx
+++ src/mycomponent/Users/Users.jsx
@@ -4,20 +4,17 @@ import * as axios from "axios";
 
 
 class Users extends React.Component {
-  constructor(props){
-        super(props);
-       axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
-          this.props.setUsers(response.data.items)
-        });
+  componentDidMount(){
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
+      this.props.setUsers(response.data.items)
+    });
+
   }
-
-
 
 
 render(){
   return<div>
-    <button onClick={this.getUsers}>getUsers </button>
-    {this.props.users.map( u => <div className={style.main} key={u.id}>
+        {this.props.users.map( u => <div className={style.main} key={u.id}>
       <div className={style.left}>
         <div><img src={u.photos.small !==null ? u.photos.small: "cris.jpg" } alt=""/>
         </div>
